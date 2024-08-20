75fa0dd869ec4570f839b542f9627e22db9f0840
joeban <joeban@twbeex.com>
2024-08-20 17:16:20 +0800

    fix: app.ts add middleware next()

--- app.ts
+++ app.ts
@@ -3,9 +3,10 @@ import { Application } from "https://deno.land/x/oak@v16.1.0/mod.ts";
 import todoRoutes from "./routes/todoList.ts";
 const app = new Application();
 
-// app.use((ctx) => {
-//   ctx.response.body = "Hello CFW!";
-// });
+app.use(async (ctx, next) => {
+  console.log("Middleware!!");
+  await next();
+});
 
 app.use(todoRoutes.routes());
 app.use(todoRoutes.allowedMethods());
